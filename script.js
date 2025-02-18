34096aa4552f63a8de49eb4b915a937379b2f9f7
Mimmi Eriksson <mimmi.aj.eriksson@gmail.com>
2025-02-18 16:48:52 +0100

    adding a working hamburger menu

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
-
 // Initialize variables
 let featuredUnicorn = ""
+const menu = document.querySelector(".menu")
+const hamburger = document.getElementById("hamburger")
+const hamburgerIcon = document.querySelector(".hamburger-icon")
+const menuItems = document.querySelectorAll(".menu-item")
+
+console.log(menuItems)
+
+// function to toogle the hamburger menu
+const toggleMenu = () => {
+  // if the menu is visible - hide the menu
+  if (menu.classList.contains("show-menu")) {
+    menu.classList.remove("show-menu")
+    hamburgerIcon.classList.remove("closing-icon")
+  }
+  // if the menu is hidden - show the menu
+  else {
+    menu.classList.add("show-menu")
+    hamburgerIcon.classList.add("closing-icon")
+  }
+}
+
+// iterate through the links to hide the menu when a link is clicked
+menuItems.forEach(
+  function (menuItem) {
+    menuItem.addEventListener("click", toggleMenu);
+  }
+)
 
 // function to change the featured unicorn
 const changeFeaturedUnicorn = (unicornId) => {
@@ -9,7 +35,7 @@ const changeFeaturedUnicorn = (unicornId) => {
     featuredUnicorn.classList.remove("featured-unicorn")
   }
   // if the unicornId is not blank - assign the new featured unicorn and add the class to the new unicorn
-  if (featuredUnicorn) {
+  if (unicornId) {
     featuredUnicorn = document.getElementById(unicornId)
     featuredUnicorn.classList.add("featured-unicorn")
   }
